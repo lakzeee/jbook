ee63df502589b58680146f7ac08e3e76cf74b8af
lak <caizh1997@live.com>
2023-05-05 22:27:23 -0500

    implement redux for project

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { createRoot } from "react-dom/client";
 import TextEditor from "./components/text-editor";
+import { Provider } from "react-redux";
+import { store } from "./state";
 // import CodeCell from "./components/code-cell";
 
 function App() {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 }
 const rootElement = document.getElementById("root");
